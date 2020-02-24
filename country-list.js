ddc9ab9bd649f5e4e502996e91c35eb0e3a9081a
Yurii Mudryi <yurii.mudryi@elopage.com>
2020-02-24 16:08:12 +0200

    fixed default export

--- country-list.js
+++ country-list.js
@@ -12,7 +12,9 @@ function mapCodeAndName (country) {
   codeMap[country.alpha2.toLowerCase()] = country.name
 }
 
-exports.overwrite = function overwrite (countries) {
+module.exports = data
+
+module.exports.overwrite = function overwrite (countries) {
   if (!countries || !countries.length) return
   countries.forEach(function (country) {
     var foundIndex = data.findIndex(function (item) {
@@ -23,40 +25,40 @@ exports.overwrite = function overwrite (countries) {
   })
 }
 
-exports.getCode = function getCode (name) {
+module.exports.getCode = function getCode (name) {
   return nameMap[name.toLowerCase()]
 }
 
-exports.getName = function getName (alpha2) {
+module.exports.getName = function getName (alpha2) {
   return codeMap[alpha2.toLowerCase()]
 }
 
-exports.getNames = function getNames () {
+module.exports.getNames = function getNames () {
   return data.map(function (country) {
     return country.name
   })
 }
 
-exports.getCodes = function getCodes () {
+module.exports.getCodes = function getCodes () {
   return data.map(function (country) {
     return country.alpha2
   })
 }
 
-exports.getCodeList = function getCodeList () {
+module.exports.getCodeList = function getCodeList () {
   return codeMap
 }
 
-exports.getNameList = function getNameList () {
+module.exports.getNameList = function getNameList () {
   return nameMap
 }
 
-exports.add = function add (country, name, euMember) {
+module.exports.add = function add (country, name, euMember) {
   if (!name) data.push(country)
   else data.push({ alpha2: country, name, euMember })
 }
 
-exports.remove = function remove (id) {
+module.exports.remove = function remove (id) {
   if (typeof id === 'number') data.splice(id, 1)
   if (typeof id === 'string') {
     var foundIndex = data.findIndex(function (item) {
@@ -65,5 +67,3 @@ exports.remove = function remove (id) {
     data.splice(foundIndex, 1)
   }
 }
-
-exports.default = data
